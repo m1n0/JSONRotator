5456ca860baa6055fc9528e30b0812fb49a67564
m1n0 <mino.lukac@gmail.com>
2016-06-20 16:49:55 +0200

    Fixed PHPSpec after moving classes

---
+++ spec/m1n0/JSONRotatorSpec.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace spec\m1n0;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class JSONRotatorSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('m1n0\JSONRotator');
+    }
+
+    function it_rotates_json()
+    {
+        $input = [
+            ["name" => "Jeffrey Scott", "company" => "Rhyloo", "country" => "Indonesia"],
+            ["name" => "Robert Hayes", "company" => "Roodel", "country" => "Brazil"],
+            ["name" => "Juan Wells", "company" => "Jabberstorm", "country" => "Cape Verde"],
+        ];
+
+        $output = [
+            'name' => ['Jeffrey Scott', 'Robert Hayes', 'Juan Wells'],
+            'company' => ['Rhyloo', 'Roodel', 'Jabberstorm'],
+            'country' => ['Indonesia', 'Brazil', 'Cape Verde'],
+        ];
+
+        $this->rotate($input)->shouldReturn($output);
+    }
+}
