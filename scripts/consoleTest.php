0810d7f55a61d356805cc47f30b883c1d5bdb36f
m1n0 <mino.lukac@gmail.com>
2016-06-20 15:58:43 +0200

    Proper mock data

--- scripts/consoleTest.php
+++ scripts/consoleTest.php
@@ -5,8 +5,16 @@ $autoloader = require __DIR__ . '/../vendor/autoload.php';
 use m1n0\JSONRotator;
 use m1n0\ConsoleTester;
 
+$dataShort = json_decode(file_get_contents(__DIR__ . '/../resources/MockDataShort.json'), true);
+$dataLong = json_decode(file_get_contents(__DIR__ . '/../resources/MockDataLong.json'), true);
+
 $rotator = new JSONRotator();
 $consoleTest = new ConsoleTester();
 
-$runOriginal = $consoleTest->run($rotator, $data, FALSE);
-$runTransposed = $consoleTest->run($rotator, $data, TRUE, $runOriginal);
+echo "Running with 10 records:\n\r";
+$runShortOriginal = $consoleTest->run($rotator, $dataShort, FALSE);
+$runShortTransposed = $consoleTest->run($rotator, $dataShort, TRUE, $runShortOriginal);
+
+echo "Running with 1000 records:\n\r";
+$runLongOriginal = $consoleTest->run($rotator, $dataLong, FALSE);
+$runLongTransposed = $consoleTest->run($rotator, $dataLong, TRUE, $runLongOriginal);
